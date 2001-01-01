f10e48431c5b88884f17bb028335319e8b8b2016
secalpha <secalpha@zk35.de>
2001-01-01 00:00:00 +0000

    refactor(db): add profiles.go with profiles, pages, preferences, search engines

---
+++ internal/db/profiles.go
@@ -0,0 +1,223 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func GetProfiles() ([]Profile, error) {
+	rows, err := DB.Query(`SELECT id, slug, name, is_default, sort_order FROM profiles ORDER BY sort_order ASC`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var profiles []Profile
+	for rows.Next() {
+		var p Profile
+		var isDefault int
+		if err := rows.Scan(&p.ID, &p.Slug, &p.Name, &isDefault, &p.SortOrder); err != nil {
+			return nil, err
+		}
+		p.IsDefault = (isDefault == 1)
+		profiles = append(profiles, p)
+	}
+	return profiles, nil
+}
+
+func GetDefaultProfile() (*Profile, error) {
+	row := DB.QueryRow(`SELECT id, slug, name, is_default, sort_order FROM profiles WHERE is_default = 1`)
+	var p Profile
+	var isDefault int
+	if err := row.Scan(&p.ID, &p.Slug, &p.Name, &isDefault, &p.SortOrder); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	p.IsDefault = (isDefault == 1)
+	return &p, nil
+}
+
+func GetProfileBySlug(slug string) (*Profile, error) {
+	row := DB.QueryRow(`SELECT id, slug, name, is_default, sort_order FROM profiles WHERE slug = ?`, slug)
+	var p Profile
+	var isDefault int
+	if err := row.Scan(&p.ID, &p.Slug, &p.Name, &isDefault, &p.SortOrder); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	p.IsDefault = (isDefault == 1)
+	return &p, nil
+}
+
+func AddProfile(name, slug string) error {
+	_, err := DB.Exec(`INSERT INTO profiles (slug, name, sort_order) VALUES (?, ?, (SELECT COALESCE(MAX(sort_order), 0) + 1 FROM profiles))`, slug, name)
+	return err
+}
+
+func DeleteProfile(slug string) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	var isDefault int
+	err = tx.QueryRow(`SELECT is_default FROM profiles WHERE slug = ?`, slug).Scan(&isDefault)
+	if err != nil {
+		return err
+	}
+	if isDefault == 1 {
+		return fmt.Errorf("cannot delete default profile")
+	}
+
+	if _, err := tx.Exec(`DELETE FROM visibility WHERE profile = ?`, slug); err != nil {
+		return err
+	}
+	if _, err := tx.Exec(`DELETE FROM user_preferences WHERE profile = ?`, slug); err != nil {
+		return err
+	}
+	if _, err := tx.Exec(`DELETE FROM user_settings WHERE profile = ?`, slug); err != nil {
+		return err
+	}
+	if _, err := tx.Exec(`DELETE FROM widgets WHERE profile = ?`, slug); err != nil {
+		return err
+	}
+	if _, err := tx.Exec(`DELETE FROM profiles WHERE slug = ?`, slug); err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func SetDefaultProfile(slug string) error {
+	tx, err := DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	if _, err := tx.Exec(`UPDATE profiles SET is_default = 0 WHERE is_default = 1`); err != nil {
+		return err
+	}
+
+	res, err := tx.Exec(`UPDATE profiles SET is_default = 1 WHERE slug = ?`, slug)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return fmt.Errorf("profile with slug %s not found", slug)
+	}
+
+	return tx.Commit()
+}
+
+func GetPages(profile string) ([]Page, error) {
+	rows, err := DB.Query(`SELECT id, profile, name, icon, sort_order FROM pages WHERE profile = ? ORDER BY sort_order ASC`, profile)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var pages []Page
+	for rows.Next() {
+		var p Page
+		if err := rows.Scan(&p.ID, &p.Profile, &p.Name, &p.Icon, &p.SortOrder); err != nil {
+			return nil, err
+		}
+		pages = append(pages, p)
+	}
+	return pages, nil
+}
+
+func AddPage(profile, name, icon string) (int64, error) {
+	res, err := DB.Exec(`INSERT INTO pages (profile, name, icon, sort_order) VALUES (?, ?, ?, (SELECT COALESCE(MAX(sort_order), 0) + 1 FROM pages WHERE profile = ?))`, profile, name, icon, profile)
+	if err != nil {
+		return 0, err
+	}
+	return res.LastInsertId()
+}
+
+func DeletePage(id int) error {
+	_, err := DB.Exec(`DELETE FROM pages WHERE id = ?`, id)
+	return err
+}
+
+func UpdatePage(id int, name, icon string) error {
+	_, err := DB.Exec(`UPDATE pages SET name=?, icon=? WHERE id=?`, name, icon, id)
+	return err
+}
+
+func UpdatePageSort(id, sortOrder int) error {
+	_, err := DB.Exec(`UPDATE pages SET sort_order = ? WHERE id = ?`, sortOrder, id)
+	return err
+}
+
+func GetUserPreferences(profile string) (*UserPreferences, error) {
+	p := &UserPreferences{
+		Profile:        profile,
+		Theme:          "dark",
+		AccentColor:    "#6366f1",
+		SearchEngine:   "https://duckduckgo.com/",
+		Background:     "aurora",
+		Language:       "de",
+		Layout:         "grid",
+		BackgroundMode: "aurora",
+	}
+	row := DB.QueryRow(`SELECT theme, accent_color, search_engine, background, language, layout, COALESCE(custom_css,''), COALESCE(background_mode,'aurora')
+		FROM user_preferences WHERE profile = ?`, profile)
+	err := row.Scan(&p.Theme, &p.AccentColor, &p.SearchEngine, &p.Background, &p.Language, &p.Layout, &p.CustomCSS, &p.BackgroundMode)
+	if err == sql.ErrNoRows {
+		return p, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func SetUserPreferences(profile string, prefs UserPreferences) error {
+	_, err := DB.Exec(`INSERT INTO user_preferences (profile, theme, accent_color, search_engine, background, language, layout, custom_css, background_mode)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+		ON CONFLICT(profile) DO UPDATE SET
+			theme = excluded.theme,
+			accent_color = excluded.accent_color,
+			search_engine = excluded.search_engine,
+			background = excluded.background,
+			language = excluded.language,
+			layout = excluded.layout,
+			custom_css = excluded.custom_css,
+			background_mode = excluded.background_mode`,
+		profile, prefs.Theme, prefs.AccentColor, prefs.SearchEngine,
+		prefs.Background, prefs.Language, prefs.Layout, prefs.CustomCSS, prefs.BackgroundMode)
+	return err
+}
+
+func GetSearchEngine(profile string) string {
+	var url string
+	err := DB.QueryRow(`SELECT search_engine FROM user_settings WHERE profile = ?`, profile).Scan(&url)
+	if err != nil || url == "" {
+		return "https://duckduckgo.com/"
+	}
+	return url
+}
+
+func SetSearchEngine(profile, engineURL string) error {
+	_, err := DB.Exec(`INSERT INTO user_settings (profile, search_engine) VALUES (?, ?)
+		ON CONFLICT(profile) DO UPDATE SET search_engine = ?`, profile, engineURL, engineURL)
+	return err
+}
+
+func GetAllSearchEngines() map[string]string {
+	profiles, _ := GetProfiles()
+	result := make(map[string]string)
+	for _, p := range profiles {
+		result[p.Slug] = GetSearchEngine(p.Slug)
+	}
+	return result
+}
